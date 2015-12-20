004eb74fbe72011bc448ae18b9f74458be9cc8c5
Eric Bidelman <ebidel@gmail.com>
2015-12-20 13:48:58 -0800

    Init commit

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+/*
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+'use strict';
+
+let del = require('del');
+let gulp = require('gulp');
+let $ = require('gulp-load-plugins')();
+// let crisper = require('gulp-crisper');
+// let license = require('gulp-license');
+// let vulcanize = require('gulp-vulcanize');
+let polybuild = require('polybuild');
+let merge = require('merge-stream');
+let runSequence = require('run-sequence');
+
+function minifyHtml() {
+  return $.minifyHtml({quotes: true, empty: true, spare: true});
+}
+
+function uglifyJS() {
+  return $.uglify({preserveComments: 'some'});
+}
+
+function license() {
+  return $.license('BSD-3-Clause', {
+    organization: 'The Polymer Project Authors. All rights reserved.',
+    tiny: true
+  });
+}
+
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('copy', function() {
+  let docs = gulp.src([
+      '*.html'
+     ], {base: '.'})
+    .pipe(gulp.dest('dist'));
+
+  let bower = gulp.src([
+    'bower_components/webcomponentsjs/webcomponents-lite*.js'
+    ], {base: '.'})
+    .pipe(gulp.dest('dist'));
+
+  return merge(docs, bower);
+});
+
+gulp.task('vulcanize', function() {
+  return gulp.src('elements/elements.html')
+    // .pipe($.vulcanize({
+    //   inlineScripts:true,
+    //   inlineCss: false,
+    //   stripComments: true
+    // }))
+    // .pipe($.crisper({scriptInHead: true}))
+    // .pipe($.if('*.html', minifyHtml())) // Minify html output
+    // .pipe($.if('*.js', uglifyJS())) // Minify js output
+    .pipe(polybuild({
+      maximumCrush: true,
+      suffix: ''
+    }))
+    .pipe($.if('*.js', license()))
+    .pipe(gulp.dest('dist/elements'));
+});
+
+gulp.task('default', ['clean'], function(done) {
+  runSequence(
+    ['vulcanize', 'copy'],
+    done);
+});
